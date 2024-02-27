5a4b60e53e935e70eebff5b96285e26739d2b0e7
garishma <garishmadondapati@gmail.com>
2024-02-27 11:16:57 +0530

    converting to string

---
+++ string5.cjs
@@ -0,0 +1,23 @@
+// ==== String Problem #5 ====
+// Given an array of strings ["the", "quick", "brown", "fox"], convert it into a string "the quick brown fox."
+// If the array is empty, return an empty string.
+
+function convertingToString(array){
+    if(array.length===0){
+        return " "
+    }else{
+        let res='';
+        array.forEach(element => {
+            res+=element+" "
+            
+        });
+
+        return `${res}.`;
+
+
+    }
+}
+
+module.exports=convertingToString;
+
+console.log(convertingToString(["the", "quick", "brown", "fox"]));
